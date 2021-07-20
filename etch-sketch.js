71daea63e9ebe2a58e0cd5f50b7a928686813b13
danielcr0995 <daniel.correa.1995@gmail.com>
2021-07-20 15:24:25 -0500

    addend event to change color when hover over a square

--- etch-sketch.js
+++ etch-sketch.js
@@ -1,11 +1,18 @@
 
-let number_squares=50;
+let number_squares=16;
 let totalsize=number_squares*number_squares;
 let draw_space=600;
 let square_size=600/number_squares;
+let color='blue'
 
 
-create_grid(bumber_squares,'drawing');
+create_grid(number_squares,'drawing');
+
+// let hoversq=document.querySelector('.square');
+// hoversq.addEventListener('mouseover',function(e){
+//     // console.log('click');
+//     e.style.backgroundColor=`blue`;
+// })
 
 function create_grid(number_squares, elementid){
     gridstyle(number_squares, elementid);
@@ -15,6 +22,11 @@ function create_grid(number_squares, elementid){
         let square=document.createElement('div');
         square.classList.add('square');
         square.style.border='0.5px solid rgb(75, 75, 75)';
+        square.addEventListener('mouseover',function(e){
+            // console.log('click');
+            e.target.style.backgroundColor=`blue`;
+        })
+        // square.textContent='.';
         draw.appendChild(square);
     }
 }
